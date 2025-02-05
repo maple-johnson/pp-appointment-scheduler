8d8fd298cb23f0c5f50bd9179cb131dc2d409b55
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-02-05 14:55:38 -0800

    Added post and saved form into an array

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 const PORT = 3000;
+const apptList = [];
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/send-form', (req, res) => {
+    //console.log(req.body);
+    apptList.push(req.body);
+    res.send(`<h1>Confirmed! See you then ${req.body.fname}!</h1>`);
 });
 
 app.listen(PORT, () => {
